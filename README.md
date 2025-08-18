c72495381201ec3f5cd7e448a9c8058a90e07a57
DanielCDoran <40theridgeway@gmail.com>
2025-08-18 13:09:32 +0000

    seems to work with animal

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+npm run build
+npm run start:cli hello runs the hello command
+
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
--- src/hello.command.ts
+++ src/hello.command.ts
@@ -1,5 +1,6 @@
 import { Command, CommandRunner, Option } from 'nest-commander';
 import { AppService } from './app.service';
+import { Animal, Snake, Horse } from "./animal";
 
 @Command({ name: 'hello', description: 'a hello command' })
 export class HelloCommand extends CommandRunner {
@@ -9,6 +10,11 @@ export class HelloCommand extends CommandRunner {
 
   async run(): Promise<void> {
     console.log(this.appService.getHello());
+    let sam = new Snake("Sammy the Python");
+    let tom: Animal = new Horse("Tommy the Palomino");
+
+    sam.move();
+    tom.move(34);
   }
 
   @Option({
